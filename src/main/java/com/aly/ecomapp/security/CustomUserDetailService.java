3f57c6a746d6728f70e356c8aedbd6d379efd73c
AlyTamer <35073465+AlyTamer@users.noreply.github.com>
2025-08-03 13:23:22 +0300

    implement JWT authentication filter and user detail service; add custom exceptions for JWT and user handling

--- src/main/java/com/aly/ecomapp/security/CustomUserDetailService.java
+++ src/main/java/com/aly/ecomapp/security/CustomUserDetailService.java
@@ -1,16 +1,40 @@
 package com.aly.ecomapp.security;
 
+import com.aly.ecomapp.UserRepo;
+import com.aly.ecomapp.exceptions.UserException;
+import com.aly.ecomapp.exceptions.UserExceptionMessages;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.List;
+
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
+    private final UserRepo userRepo;
+
+    @Autowired
+    public CustomUserDetailService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User appUser = userRepo.findByUserName(username);
+        if (appUser == null) {
+            throw new UserException(UserExceptionMessages.userNotFound);
+        }
+
+
+        return new org.springframework.security.core.userdetails.User(
+                appUser.getUsername(),
+                appUser.getPassword(),
+                List.of(new SimpleGrantedAuthority(appUser.getRoles().toString())));
     }
 }
+
