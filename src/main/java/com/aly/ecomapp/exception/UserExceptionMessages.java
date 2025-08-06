87f8ceb24aa1a93635458294c63e5ea8f4591429
Aly <aly.t.elenany@gmail.com>
2025-08-06 15:13:01 +0300

    refactor exception messages: update comment for clarity and modify .gitignore to include testing directory

--- src/main/java/com/aly/ecomapp/exception/UserExceptionMessages.java
+++ src/main/java/com/aly/ecomapp/exception/UserExceptionMessages.java
@@ -1,12 +1,7 @@
 package com.aly.ecomapp.exception;
 
-//Exception messages for user-related errors
+//Exception messages for user related errors
 public class UserExceptionMessages {
     public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
-    public static final String USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
     public static final String INVALID_CREDENTIALS = "INVALID_CREDENTIALS";
-    public static final String UNAUTHORIZED_ACCESS = "UNAUTHORIZED_ACCESS";
-    public static final String ACCOUNT_LOCKED = "ACCOUNT_LOCKED";
-    public static final String EMAIL_ALREADY_IN_USE = "EMAIL_ALREADY_IN_USE";
-    public static final String INVALID_EMAIL_FORMAT = "INVALID_EMAIL_FORMAT";
 }
